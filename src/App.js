ac44b374d44ffd44c28dbdc6629f0303905dc9ae
Joe Dahle <joe.dahle@gmail.com>
2019-03-20 12:54:21 -0700

    update sig

--- src/App.js
+++ src/App.js
@@ -4,38 +4,35 @@ import fb from "./images/facebook.png";
 import tw from "./images/twitter.png";
 import ig from "./images/instagram.png";
 import yt from "./images/youtube.png";
+import title from "./images/JessieJacobsVizRTArt.png";
+import shape from "./images/Shape1.png";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="content">
-            <img className="logo" src={logo} alt="The List Logo" />
-            <div className="desc">
-              <p className="name">Joe</p>
-              <p>IT Operations</p>
-              <p>
-                <a href="mailto:joe@email.com">joe@email.com</a>
-              </p>
-              <p>Office: 608.914.3240 | Cell: 480.431.7659</p>
-            </div>
+        <div id="background">
+          <div id="TheListLogo100x100">
+            <img src={logo} />
           </div>
-          <hr />
-          <div className="social">
-            <a href="https://facebook.com">
-              <img src={fb} alt="facebook" />
-            </a>
-            <a href="https://twitter.com">
-              <img src={ig} alt="instagram" />
-            </a>
-            <a href="https://twitter.com">
-              <img src={tw} alt="twitter" />
-            </a>
-            <a href="https://twitter.com">
-              <img src={yt} alt="youtube" />
-            </a>
+          <div id="facebook">
+            <img src={fb} />
+          </div>
+          <div id="instagram">
+            <img src={ig} />
+          </div>
+          <div id="twitter">
+            <img src={tw} />
+          </div>
+          <div id="youtube">
+            <img src={yt} />
+          </div>
+          <div id="JessieJacobsVizRTArt">
+            <img src={title} />
+          </div>
+          <div id="Shape1">
+            <img src={shape} />
           </div>
         </div>
       </div>
